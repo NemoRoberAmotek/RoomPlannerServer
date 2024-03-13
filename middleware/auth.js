131d26fffc5b5e5e6bdb9ef9759deb4059ccc635
Nemo Rober <nemo.rober@gmail.com>
2024-03-13 07:32:26 +0100

    adds .env file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,21 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require("jsonwebtoken")
 
 module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+    const token = req.header("x-auth-token")
 
-  if (!token) {
-    return res.status(401).json({ msg: "No token, authorization denied." });
-  }
+    if (!token) {
+        return res.status(401).json({ msg: "No token, authorization denied." })
+    }
 
-  try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    req.user = decoded.user;
+        req.user = decoded.user
 
-    next();
-  } catch (err) {
-    res.status(401).json({
-      msg: "Token is not valid.",
-    });
-  }
-};
+        next()
+    } catch (err) {
+        res.status(401).json({
+            msg: "Token is not valid.",
+        })
+    }
+}
--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const cors = require("cors");
+const express = require("express")
+const connectDB = require("./config/db")
+const cors = require("cors")
 
-const app = express();
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express()
 
 const corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-};
+    origin: process.env.CLIENT_HOST,
+    optionsSuccessStatus: 200,
+}
 
 //Connect Database
-connectDB();
+connectDB()
 
 //Init Middleware
-app.use(express.json({ extended: false }));
-app.use(cors(corsOptions));
+app.use(express.json({ extended: false }))
+app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
-  res.send("API Running...");
-});
+    res.send("API Running...")
+})
 
 //Define Routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/rooms", require("./routes/api/rooms"));
-app.use("/api/furniture", require("./routes/api/furniture"));
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/rooms", require("./routes/api/rooms"))
+app.use("/api/furniture", require("./routes/api/furniture"))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
-});
+    console.log(`Server started on ${PORT}`)
+})
